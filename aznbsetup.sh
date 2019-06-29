9bd0543983b9273c0beb6dc291a1a407b8739020
Herman Tolentino <hermantolentino@users.noreply.github.com>
2019-06-28 22:21:37 -0400

    Update aznbsetup.sh

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -6,8 +6,26 @@ source /home/nbuser/anaconda3_501/bin/activate
 # Install packages
 conda config --add channels conda-forge
 conda update -y  conda
+conda remove -y conda-build
 conda install -y \
   folium=0.9* \
   pandas=0.24* \
   osmnx=0.1* \
   geopandas=0.5*
+
+/home/nbuser/anaconda3_501/bin/deactivate
+
+# Activate environment
+source /home/nbuser/anaconda3_420/bin/activate
+
+# Install packages
+conda config --add channels conda-forge
+conda update -y  conda
+conda remove -y conda-build
+conda install -y \
+  folium=0.9* \
+  pandas=0.24* \
+  osmnx=0.1* \
+  geopandas=0.5*
+
+/home/nbuser/anaconda3_420/bin/deactivate
