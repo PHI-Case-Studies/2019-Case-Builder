80ceda6399e8ba9d061276afe349f1fa20ef3853
Herman Tolentino <hermantolentino@users.noreply.github.com>
2019-06-30 08:37:03 -0400

    Update aznbsetup.sh

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -3,6 +3,12 @@
 # Activate environment
 source /home/nbuser/anaconda3_501/bin/activate
 
+# Set up proxy
+http_proxy=http://webproxy:3128
+https_proxy=http://webproxy:3128
+export http_proxy
+export https_proxy
+
 # Install packages
 conda update -n base -c conda-forge conda conda-build
 conda install -y -c conda-forge \
@@ -15,6 +21,12 @@ source /home/nbuser/anaconda3_501/bin/deactivate
 
 # Activate environment
 source /home/nbuser/anaconda3_420/bin/activate
+
+# Set up proxy
+http_proxy=http://webproxy:3128
+https_proxy=http://webproxy:3128
+export http_proxy
+export https_proxy
 
 # Install packages
 conda update -n base -c conda-forge conda conda-build
