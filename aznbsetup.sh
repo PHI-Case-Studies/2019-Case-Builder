1deee2f32b55da4c3b78059c6dbfdbc03030c46e
Herman Tolentino <hermantolentino@users.noreply.github.com>
2019-06-30 22:48:20 -0400

    Update aznbsetup.sh

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -13,6 +13,7 @@ source /home/nbuser/anaconda3_420/bin/activate
 conda update -c conda-forge conda conda-build
 conda install -y -c conda-forge osmnx=0.1* folium=0.9.1
 
+pip install --upgrade pip
 pip install pandas==0.24.2
 
 source /home/nbuser/anaconda3_420/bin/deactivate
