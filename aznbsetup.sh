12ec6b73651d3cc121a5eca08f47c5f7882b5c73
Herman Tolentino <hermantolentino@users.noreply.github.com>
2019-08-09 16:10:17 +0700

    Update aznbsetup.sh

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -3,15 +3,9 @@
 # Activate environment
 source /home/nbuser/anaconda3_420/bin/activate
 
-# Set up proxy
-#http_proxy=http://webproxy:3128
-#https_proxy=http://webproxy:3128
-#export http_proxy
-#export https_proxy
-
 # Install packages
 conda update -c conda-forge conda conda-build
-conda install -y -c conda-forge osmnx=0.10* folium=0.10* pandas=0.5* geopandas=0.5*
+conda install -y -c conda-forge osmnx=0.10* folium=0.10* pandas=0.25* geopandas=0.5*
 
 pip install --upgrade pip
 
